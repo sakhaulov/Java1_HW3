0dafc684d9c774fc574401087b1ce6acdaf0a2ce
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2021-12-12 16:13:59 +0500

    Removed | from printArray output

--- src/Main.java
+++ src/Main.java
@@ -68,7 +68,7 @@ public class Main {
     public static void printArray(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j]+ " | ");
+                System.out.print(array[i][j]+ "\t");
             }
             System.out.println();
         }
